d157bedf2220dbf263daa5b92e648436d0f35cd8
josefkerner <kerner.jo@gmail.com>
2024-08-28 12:42:52 +0200

    init

---
+++ packages/858602710103_helmets/src/test.py
@@ -0,0 +1,14 @@
+import panoramasdk as p
+
+import logging
+from logging.handlers import RotatingFileHandler
+log = logging.getLogger('my_logger')
+log.setLevel(logging.DEBUG)
+import sys
+import cv2, math
+
+
+
+
+from ultralytics import YOLO
+model = YOLO("/panorama/best.pt")
