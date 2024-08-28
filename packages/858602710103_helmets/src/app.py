950e9332a037e6c99ecfa7a8ca0c4d6d0e5ce3e3
josefkerner <kerner.jo@gmail.com>
2024-08-28 12:39:15 +0200

    init

--- packages/858602710103_helmets/src/app.py
+++ packages/858602710103_helmets/src/app.py
@@ -1,3 +1,10 @@
+import panoramasdk as p
+
+import logging
+from logging.handlers import RotatingFileHandler
+log = logging.getLogger('my_logger')
+log.setLevel(logging.DEBUG)
+import sys
 import cv2, math
 
 
@@ -6,97 +13,26 @@ import cv2, math
 from ultralytics import YOLO
 model = YOLO("/panorama/best.pt")
 
-# start webcam
-cap = cv2.VideoCapture(0)
-cap.set(3, 640)
-cap.set(4, 480)
-#model = YOLO("models/hemletYoloV8_100epochs.pt")
+class ObjectDetectionApp(p.node):
 
-print(model.names)
+        def __init__(self):
+            self.pre_processing_output_size = 640
 
+        def get_frames(self):
+            input_frames = self.inputs.video_in.get()
+            return input_frames
 
+        def run(self):
+            log.info("Pytorch Yolov5s FP16 App starts")
+            image_list = [] # An image queue
+        def get_frames(self):
+            input_frames = self.inputs.video_in.get()
+            return input_frames
 
-'''
-classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
-              "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
-              "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella",
-              "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat",
-              "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup",
-              "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli",
-              "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed",
-              "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone",
-              "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors",
-              "teddy bear", "hair drier", "toothbrush"
-              ]
-              
-'''
-
-classNames = [
-    "helmet", "head", "person"
-]
-
-
-
-import cv2
-
-while True:
-    success, img = cap.read()
-    results = model(img, stream=True)
-
-    print(results)
-
-    # coordinates
-    for r in results:
-        boxes = r.boxes
-
-        coords = []
-        objects = []
-
-        for box in boxes:
-            # bounding box
-            x1, y1, x2, y2 = box.xyxy[0]
-            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
-
-            # put box in cam
-            cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
-
-            # confidence
-            confidence = math.ceil((box.conf[0]*100))/100
-            print("Confidence --->",confidence)
-
-            # class name
-            cls = int(box.cls[0])
-            if cls > len(classNames):
-                obj = "Unknown"
-                print("Class name -->", "Unknown")
-            else:
-                obj = model.names[cls]
-                objects.append(obj)
-                #print("Class name -->", classNames[cls])
-
-            # object details
-            org = [x1, y1]
-            coords.append([x1,y1])
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        fontScale = 1
-        color = (255, 0, 0)
-        thickness = 2
-        if len(coords) > 0:
-            if len(coords) > 1:
-                distance = math.sqrt((coords[0][0] - coords[1][0])**2 + (coords[0][1] - coords[1][1])**2)
-            else:
-                distance = 'no'
-
-            if len(objects) == 1 and objects[0] == 'helmet':
-                txt = f"Wears helmet"
-            else:
-                txt = f"No helmet"
-
-            cv2.putText(img, txt, coords[0], font, fontScale, color, thickness)
-
-    cv2.imshow('Webcam', img)
-    if cv2.waitKey(1) == ord('q'):
-        break
-
-cap.release()
-cv2.destroyAllWindows()
+if __name__ == '__main__':
+    try:
+        app = ObjectDetectionApp()
+        app.run()
+    except Exception as err:
+        log.exception('App did not Start {}'.format(err))
+        sys.exit(1)
