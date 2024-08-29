1d23f72c34a4ab4c4390ce5d2fae8c82146f6827
josefkerner <kerner.jo@gmail.com>
2024-08-29 20:16:21 +0200

    init

--- scripts/testrun.sh
+++ scripts/testrun.sh
@@ -1,4 +1,1 @@
-python3 ../aws-panorama-samples/samples/common/test_utility/panorama_test_utility_run.py \
-    --app-name helmets \
-    --code-package-name helmets \
-    --py-file app.py
+python3 ../aws-panorama-samples/samples/common/test_utility/panorama_test_utility_run.py --app-name helmets --code-package-name helmets --py-file app.py
