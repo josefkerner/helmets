585c50520d19dcf170ee2daf36224c810ffac677
josefkerner <kerner.jo@gmail.com>
2024-08-29 20:56:10 +0200

    init

--- packages/858602710103-helmets-1.0/src/app/app.py
+++ packages/858602710103-helmets-1.0/src/app/app.py
@@ -15,7 +15,9 @@ classNames = [
 
 
 from ultralytics import YOLO
-model = YOLO("/panorama/app/best.pt")
+#model_path = '/panorama/app/best.pt'
+model_path = 'packages/858602710103-helmets-1.0/src/app/best.pt'
+model = YOLO(model_path)
 
 class ObjectDetectionApp(p.node):
 
