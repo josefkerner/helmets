7e7a5f163c1e50b1a3ff556bb02621d1a92d9df4
josefkerner <kerner.jo@gmail.com>
2024-08-31 22:20:33 +0200

    init

---
+++ test.py
@@ -0,0 +1,36 @@
+base = 3600000
+hypo = 31000
+interest = 0.08
+years = 30
+val = 0
+rent = 26000
+rent_interest = 0.03
+
+paid_hypo = 0
+
+for i in range(0,years):
+    print(i)
+    paid_rent_year = (rent * 12) * (1+rent_interest)
+    rent = paid_rent_year / 12
+
+    print('paid rent',paid_rent_year)
+    hypo_year = hypo * 12
+    print('hypo year',hypo_year)
+
+    diff = hypo_year - paid_rent_year
+
+    if diff < 0:
+        hypo_year = 0
+        print('RENT is bigger than hypo!')
+        exit(1)
+
+
+    paid_hypo = paid_hypo + hypo_year
+    if val == 0:
+        val = base * (1+interest) + hypo_year * (1+interest)
+    else:
+        val = val * (1+interest) + hypo_year * (1+interest)
+    print('value',val)
+    print('paid_hypo',paid_hypo)
+
+
